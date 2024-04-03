1b3eec698e201c34575fd277e20e9eccb2083815
Timothy Arlet <tarlet@cnm.edu>
2024-04-03 13:04:19 -0600

    UI in progress.

---
+++ app/src/main/java/edu/cnm/deepdive/jata/view/BoardView.java
@@ -0,0 +1,18 @@
+package edu.cnm.deepdive.jata.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+import androidx.annotation.NonNull;
+
+public class BoardView extends View {
+
+  public BoardView(Context context) {
+    super(context);
+  }
+
+  @Override
+  protected void onDraw(@NonNull Canvas canvas) {
+    super.onDraw(canvas);
+  }
+}
