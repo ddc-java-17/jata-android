fa21180fa38c4497e44f518ccf34a3148f1fe31b
DanielleScott <dmyers28@cnm.edu>
2024-04-03 18:48:03 -0600

    Started adding JavaDocs.

--- app/src/main/java/edu/cnm/deepdive/jata/model/Ship.java
+++ app/src/main/java/edu/cnm/deepdive/jata/model/Ship.java
@@ -2,6 +2,7 @@ package edu.cnm.deepdive.jata.model;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+import edu.cnm.deepdive.jata.model.entity.Board;
 
 /**
  * This is the data structure for Ship in the game. This class will store relevant data for the
@@ -24,14 +25,25 @@ public class Ship {
   @Expose
   private boolean isVertical;
 
+  /**
+   * This constructor initiates the final fields in this class.
+   *
+   * @param shipNumber The identifying number of a {@link Ship} on any given {@link Board} instance.
+   *                   Every {@code shipNumber} is unique to the {@link Board} instance, but not the
+   *                   {@link Game} instance.
+   * @param length The number that indicates the length of a particular {@link Ship}. This also
+   *               indicates the total number of hits the {@code ship} can take, and the type of
+   *               {@code ship} it is as well (Trireme vs Galley vs Canoe; basically flavor text).
+   */
   public Ship(int shipNumber, int length) {
     this.shipNumber = shipNumber;
     this.length = length;
   }
 
   /**
-   * This getter gets the {@code shipNumber}
-   * @return
+   * {@code shipNumber} is the length of a particular {@link Ship}.
+   *
+   * @return shipNumber
    */
   public int getShipNumber() {
     return shipNumber;
