13875b1c8f0f4c15f63a988ffd30d31c084843c0
DanielleScott <dmyers28@cnm.edu>
2024-04-03 20:35:56 -0600

    Added JavaDocs to Ship

--- app/src/main/java/edu/cnm/deepdive/jata/model/Ship.java
+++ app/src/main/java/edu/cnm/deepdive/jata/model/Ship.java
@@ -5,8 +5,8 @@ import com.google.gson.annotations.SerializedName;
 import edu.cnm.deepdive.jata.model.entity.Board;
 
 /**
- * This is the data structure for Ship in the game. This class will store relevant data for the
- * client side of the game.
+ * This is the domain class that represents {@link Ship} instances in the game. This class will
+ * store and transfer relevant data for the client side of the game.
  */
 public class Ship {
 
@@ -41,62 +41,73 @@ public class Ship {
   }
 
   /**
-   * {@code shipNumber} is the length of a particular {@link Ship}.
-   *
-   * @return shipNumber
+   * This method gets the {@code shipNumber} for a particular {@code ship}. This is the identifying
+   * number of a {@link Ship} on any given {@link Board} instance. Every {@code shipNumber} is
+   * unique to the {@link Board} instance, but not the {@link Game} instance.
    */
   public int getShipNumber() {
     return shipNumber;
   }
 
   /**
-   *
-   * @return
+   * This method gets the X coordinate of a {@code ship} on a board. This makes up half of a ship
+   * location on the board.
    */
   public int getShipCoordX() {
     return shipCoordX;
   }
 
   /**
+   * This method sets the X coordinate of a {@code ship} on a board. This makes up half of a ship
+   * location on the board.
    *
-   * @param shipCoordX
+   * @param shipCoordX The X coordinate of a {@code ship} on a board.
    */
   public void setShipCoordX(int shipCoordX) {
     this.shipCoordX = shipCoordX;
   }
 
   /**
-   *
-   * @return
+   * This method gets the Y coordinate of a {@code ship} on a board. This makes up half of a ship
+   * location on the board.
    */
   public int getShipCoordY() {
     return shipCoordY;
   }
 
   /**
+   * This method sets the Y coordinate of a {@code ship} on a board. This makes up half of a ship
+   * location on the board.
    *
-   * @param shipCoordY
+   * @param shipCoordY The Y coordinate of a {@code ship} on a board.
    */
   public void setShipCoordY(int shipCoordY) {
     this.shipCoordY = shipCoordY;
   }
 
   /**
-   *
-   * @return
+   * This method gets {@code length}. The {@code length} is how many points on a board that a
+   * {@code ship} occupies on a board. It also indicates how many hits a ship can take and what type
+   * of {@code ship} it is.
    */
   public int getLength() {
     return length;
   }
 
   /**
-   *
-   * @return
+   * This method gets {@code isVertical}. {@code isVertical} is a boolean flag indicating if a ship
+   * has been oriented vertically or horizontally on the board.
    */
   public boolean isVertical() {
     return isVertical;
   }
 
+  /**
+   * This method sets {@code isVertical}.
+   *
+   * @param vertical This is a boolean flag indicating if a ship has been oriented vertically or
+   *                 horizontally on the board.
+   */
   public void setVertical(boolean vertical) {
     isVertical = vertical;
   }
